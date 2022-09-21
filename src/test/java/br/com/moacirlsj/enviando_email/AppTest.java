891d1920f18ca90955493051a7af23693440a42a
Moacir <skytec.dev@gmail.com>
2022-09-21 14:17:06 -0300

    adding prop mail.smtp.auth and .ssl.trust

--- src/test/java/br/com/moacirlsj/enviando_email/AppTest.java
+++ src/test/java/br/com/moacirlsj/enviando_email/AppTest.java
@@ -15,25 +15,25 @@ import javax.mail.internet.MimeMessage;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     private String userName = "ricaom.sepol@gmail.com";
     private String passWord = "uneohqbnddgloggy";
-	
-	@org.junit.Test
+
+    @org.junit.Test
     public void testEmail() {
-		try {
-		Properties prop = new Properties();
+        try {
+            Properties prop = new Properties();
 //		Properties prop = System.getProperties();
-		
-		prop.put("mail.smtp.auth", "true");
-		prop.put("mail.smtp.starttls", "true");
-		prop.put("mail.smtp.host", "smtp.gmail.com");
-//		prop.put("mail.smtp.ssl.enable", "true");
-		prop.put("mail.smtp.port", "465");
-		prop.put("mail.smtp.socketFactory.port", "465");
-		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		
+
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.starttls", "true");
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            prop.put("mail.smtp.ssl.enable", "true");
+            prop.put("mail.smtp.ssl.trust", "*");
+            prop.put("mail.smtp.port", "465");
+            prop.put("mail.smtp.socketFactory.port", "465");
+            prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
 //		prop.setProperty("mail.smtp.auth", "true");
 //		prop.setProperty("mail.smtp.starttls", "true");
 //		prop.setProperty("mail.smtp.host", "smtp.gmail.com");
@@ -41,33 +41,33 @@ public class AppTest
 //		prop.setProperty("mail.smtp.port", "587");
 //		prop.setProperty("mail.smtp.socketFactory.port", "465");
 //		prop.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		
-		Session sess = Session.getInstance(prop, new Authenticator() {
 
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				 
-				return new PasswordAuthentication(userName, passWord);
-			}
-			
-		});
-		
-			sess.setDebug(true);
-		
-			//Address[] toUsers = InternetAddress.parse("filho.ramos@marinha.mil.br, moaclope@gmail.com");
-			Address[] toUsers = InternetAddress.parse("moaclope@gmail.com");
-			Message message = new MimeMessage(sess);
-			message.setFrom(new InternetAddress(userName));
-			
-			message.setRecipients(Message.RecipientType.TO, toUsers);
-			message.setSubject("Teste de envio de email");
-			message.setText("Teste com a ferramente automatizada de envio de email feita em Java.");
-			Transport.send(message);
-			System.out.println("Sent message successfully...");
-		
-		} catch (AddressException e) {
-			e.printStackTrace();
-		}catch (Exception e) {
-		}
+            Session sess = Session.getInstance(prop, new Authenticator() {
+
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+
+                    return new PasswordAuthentication(userName, passWord);
+                }
+
+            });
+
+            sess.setDebug(true);
+
+
+            Address[] toUsers = InternetAddress.parse("moaclope@gmail.com");
+            Message message = new MimeMessage(sess);
+            message.setFrom(new InternetAddress(userName, "Moacir JR - Fullstack Dev"));
+
+            message.setRecipients(Message.RecipientType.TO, toUsers);
+            message.setSubject("Teste de envio de email");
+            message.setText("Teste com a ferramente automatizada de envio de email feita em Java.");
+            Transport.send(message);
+            System.out.println("Sent message successfully...");
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+        }
     }
 }
